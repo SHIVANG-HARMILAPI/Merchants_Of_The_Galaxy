dd8dac624efdcbce0f98a11a3b6910305ac2b44e
SHIVANG HARMILAPI <shivangkishanharmilapi@gmail.com>
2019-07-23 03:30:08 +0530

    Added implementation of converting roman into decimal with exception

--- Merchants_Of_The_Galaxy.Tests/UnitTest1.cs
+++ Merchants_Of_The_Galaxy.Tests/UnitTest1.cs
@@ -14,5 +14,51 @@ namespace Merchants_Of_The_Galaxy.Tests
             merchant.AssignRomanNumberToVariable("pish is X").Should().Be("X");
             merchant.AssignRomanNumberToVariable("tegj is L").Should().Be("L");
         }
+
+        [Fact]
+        public void CsubtractionIV()
+        {
+            string input = "IV";
+            var obj = new RomanToNumeralConvertor();
+            obj.Convert(input).Should().Be("4");
+        }
+
+        [Fact]
+        public void XsubtractionC()
+        {
+            string input = "XC";
+            var obj = new RomanToNumeralConvertor();
+            obj.Convert(input).Should().Be("90");
+        }
+
+        [Fact]
+        public void CsubtractionDM()
+        {
+            string input = "CDM";
+            var obj = new RomanToNumeralConvertor();
+            obj.Convert(input).Should().Be("Invalid Format");
+        }
+
+        [Fact]
+        public void IaddII()
+        {
+            string input = "III";
+            var obj = new RomanToNumeralConvertor();
+            obj.Convert(input).Should().Be("3");
+        }
+        [Fact]
+        public void Check()
+        {
+            string input = "MCMXLIV";
+            var obj = new RomanToNumeralConvertor();
+            obj.Convert(input).Should().Be("1944");
+        }
+        [Fact]
+        public void Check1()
+        {
+            string input = "MMVI";
+            var obj = new RomanToNumeralConvertor();
+            obj.Convert(input).Should().Be("2006");
+        }
     }
 }
--- Merchants_Of_The_Galaxy/Merchants.cs
+++ Merchants_Of_The_Galaxy/Merchants.cs
@@ -18,4 +18,109 @@ namespace Merchants_Of_The_Galaxy
             return _assignRomanToVariable[vararray[0]];
         }
     }
+
+    public class RomanToNumeralConvertor
+    {
+        public enum basicRomanValues{
+            I=1,
+            V=5,
+            X=10,
+            L=50,
+            C=100,
+            D=500,
+            M=1000
+
+        }
+        public string Convert(string roman)
+        {
+            roman = roman.ToUpper();
+            char ch = roman[0];
+            bool checkException = ValidRomanNumberCheck(roman);
+            if (!checkException)
+            {
+                return "Invalid Format";
+            }
+            int sum = 0;
+            for (int i = 0; i < roman.Length; i++)
+            {
+                if (i != roman.Length - 1)
+                {
+                    if (int.Parse(GetNumeral(roman[i])) < int.Parse(GetNumeral(roman[i + 1])))
+                    {
+                        sum = sum + (int.Parse(GetNumeral(roman[i + 1])) - int.Parse(GetNumeral(roman[i])));
+                        i++;
+                    }
+                    else
+                    {
+                        sum = sum + int.Parse(GetNumeral(roman[i]));
+                    }
+                }
+                else
+                {
+                    sum = sum + int.Parse(GetNumeral(roman[i]));
+                }
+            }
+            return sum.ToString();
+        }
+
+       public bool ValidRomanNumberCheck(string roman)
+        {
+            
+            if (roman.Contains("IC") || roman.Contains("IL") || roman.Contains("ID") || roman.Contains("IM"))
+            {
+                return false;
+            }
+            if (roman.Contains("XM") || roman.Contains("XD"))
+            {
+                return false;
+            }
+            if (roman.Contains("VM") || roman.Contains("LM") || roman.Contains("DM") || roman.Contains("VX"))
+            {
+                return false;
+            }
+            if (roman.Contains("IIII") || roman.Contains("XXXX") || roman.Contains("CCCC") || roman.Contains("MMMM"))
+            {
+                return false;
+            }
+            if (roman.Contains("DD") || roman.Contains("LL") || roman.Contains("VV"))
+            {
+                return false;
+            }
+            return true;
+        }
+        public string GetNumeral(char roman)
+        {
+            string numeral = "";
+            int number = 0;
+            switch (roman)
+            {
+                case 'I':number = (int)basicRomanValues.I;
+                    break;
+                case 'V':
+                    number = (int)basicRomanValues.V;
+                    break;
+                case 'X':
+                    number = (int)basicRomanValues.X;
+                    break;
+                case 'L':
+                    number = (int)basicRomanValues.L;
+                    break;
+                case 'C':
+                    number = (int)basicRomanValues.C;
+                    break;
+                case 'D':
+                    number = (int)basicRomanValues.D;
+                    break;
+                case 'M':
+                    number = (int)basicRomanValues.M;
+                    break;
+                default:
+                    number=0;
+                    break;
+            }
+            numeral = number.ToString();
+            return numeral;
+        }
+    }
+
 }
